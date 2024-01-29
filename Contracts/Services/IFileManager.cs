a6d2f71cfb9deb0ccd4b1feb0afbe453f6867168
ALEKSANDR FELYUGIN <feliugin@gmail.com>
2024-01-29 07:26:45 +0300

    Initial commit

---
+++ Contracts/Services/IFileManager.cs
@@ -0,0 +1,11 @@
+﻿namespace File.Manager
+{
+    public interface IFileManager
+    {
+        T Read<T>(string folderPath, string fileName);
+
+        void Save<T>(string folderPath, string fileName, T content);
+
+        void Delete(string folderPath, string fileName);
+    }
+}
---
+++ Services/FileManager.cs
@@ -0,0 +1,40 @@
+﻿using Newtonsoft.Json;
+using System.IO;
+using System.Text;
+
+namespace File.Manager
+{
+    public class FileManager : IFileManager
+    {
+        public T Read<T>(string folderPath, string fileName)
+        {
+            string path = Path.Combine(folderPath, fileName);
+            if (System.IO.File.Exists(path))
+            {
+                string json = System.IO.File.ReadAllText(path);
+                return JsonConvert.DeserializeObject<T>(json);
+            }
+
+            return default;
+        }
+
+        public void Save<T>(string folderPath, string fileName, T content)
+        {
+            if (!Directory.Exists(folderPath))
+            {
+                _ = Directory.CreateDirectory(folderPath);
+            }
+
+            string fileContent = JsonConvert.SerializeObject(content);
+            System.IO.File.WriteAllText(Path.Combine(folderPath, fileName), fileContent, Encoding.UTF8);
+        }
+
+        public void Delete(string folderPath, string fileName)
+        {
+            if (fileName != null && System.IO.File.Exists(Path.Combine(folderPath, fileName)))
+            {
+                System.IO.File.Delete(Path.Combine(folderPath, fileName));
+            }
+        }
+    }
+}
